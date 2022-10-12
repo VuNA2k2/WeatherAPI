99cfea651f75b2336f8f86aa2288fac87dcc58ab
NguoiLa11 <85285634+NguoiLa11@users.noreply.github.com>
2022-10-12 23:12:57 +0700

    Update admin.py

--- env/Weather/WeatherAPI/admin.py
+++ env/Weather/WeatherAPI/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Location
+from .models import Location, WeatherAPI
 # Register your models here.
+admin.site.register(WeatherAPI)
 admin.site.register(Location)
