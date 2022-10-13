809f4e9d0c83c601f66369b09cd570f11f21802c
VuNAP <vuvovan2k2>
2022-10-13 21:26:37 +0700

    update

--- env/Weather/WeatherAPI/urls.py
+++ env/Weather/WeatherAPI/urls.py
@@ -4,17 +4,16 @@ from django.urls import include, path
 from django.contrib import admin
 from rest_framework import routers
 from . import views
-from weatherAPI.views import GetAllWeatherAPI
 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path('api/locations/', views.GetAllLocationAPIView.as_view()),
     path('api/add_location/', views.AddLocationAPIView.as_view()),
     path('api/update_location/', views.UpdateLocationAPIView.as_view()),
     path('api/delete_location/', views.DeleteLocationAPIView.as_view()),
-    path('admin/', admin.site.urls)
-    path('weatherapp/', GetAllWeatherAPI.as_view()),
+    path('admin/', admin.site.urls),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- env/Weather/WeatherAPI/views.py
+++ env/Weather/WeatherAPI/views.py
@@ -2,34 +2,11 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions, response, status
 from rest_framework.views import APIView
-from .serializers import UserSerializer, GroupSerializer, GetLocationSerializer, PostLocationSerializer
+from .serializers import GetLocationSerializer, PostLocationSerializer
 
 from .models import Location
-from django.shortcuts import render
-from rest_framework.response import Response
-from .models import WeatherAPI
-from .serializers import GetAllWeattherAPISerializer, WeatherAPISerializer
 
 # Create your views here.
-class GetAllWeatherAPI(APIView):
-    def get(self, request):
-        list_weather=WeatherAPI.objects.all()
-        mydata=GetAllWeattherAPISerializer(list_weather, many=True)
-        return Response(data=mydata.data,status=status.HTTP_200_OK)
-    
-    def post(self, request):
-        mydata=WeatherAPISerializer(data=request.data)
-        if  not mydata.is_valid():
-            return Response('Sai du lieu',status=status.HTTP_400_BAD_REQUEST)
-        Temperature=mydata.data['Temperature1']
-        Windspeed=mydata.data['Windspeed1']
-        TrangThai=mydata.data['TrangThai1']
-        Id=mydata.data['Id1']
-        LocationId=mydata.data['LocationId1']
-        Date=mydata.data['Date1']
-        wt=WeatherAPI.objects.create(Temperature=Temperature,Windspeed=Windspeed,
-        TrangThai=TrangThai,Id=Id,LocationId=LocationId,Date=Date)
-        return Response(data=wt.id,status=status.HTTP_200_OK)
 
 # class UserViewSet(viewsets.ModelViewSet):
 #     """
