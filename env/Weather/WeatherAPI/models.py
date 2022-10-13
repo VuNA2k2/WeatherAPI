3f6e8c77ddd3145601d917a523e47ffde13af8fe
VuNAP <vuvovan2k2>
2022-10-13 22:58:58 +0700

    update

--- env/Weather/WeatherAPI/models.py
+++ env/Weather/WeatherAPI/models.py
@@ -2,15 +2,6 @@
 from django.db import models
 
 # Create your models here.
-
-class WeatherAPI(models.Model):
-    Temperature=models.IntegerField(default=0)
-    Windspeed=models.IntegerField(default=0)
-    TrangThai=models.CharField(max_length=255)
-    Id=models.IntegerField(default=0)
-    LocationId=models.IntegerField(default=0)
-    Date=models.CharField(max_length=255)
-    
 class Location(models.Model):
     id = models.AutoField(primary_key=True)
     country = models.CharField("Country", max_length=30)
